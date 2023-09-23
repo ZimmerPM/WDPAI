92c9265e3fc63fd9af50c61a9681e66265e6eae2
Piotr <p.zimirski@gmail.com>
2023-09-23 14:21:54 +0200

    Improved book and user removal dynamic displaying inside admin panel

--- public/js/remove-book.js
+++ public/js/remove-book.js
@@ -5,12 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const cancelDeleteButton = document.getElementById('cancelDelete');
     const modalMessageBox = deleteBookModal.querySelector('.modal-messageBox');
 
+    let bookId;
+
     function closeDeleteModal() {
         deleteBookModal.style.display = 'none';
-        modalMessageBox.innerHTML = ''; // Wyczyszczenie komunikatu
+        modalMessageBox.innerHTML = '';
+        confirmDeleteButton.disabled = false;
+        cancelDeleteButton.disabled = false;
     }
 
-    // Obsługa przycisku zamykania modala
     closeButton.addEventListener('click', closeDeleteModal);
     cancelDeleteButton.addEventListener('click', closeDeleteModal);
 
@@ -20,51 +23,49 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function refreshAfterDelay(delay) {
-        setTimeout(() => location.reload(), delay);
-    }
-
-    // Dodawanie obsługi dla każdego przycisku "Usuń"
     document.querySelectorAll(".delete-btn").forEach(function(button) {
         button.addEventListener("click", function() {
-            const bookId = this.getAttribute("data-id");
+            bookId = this.getAttribute("data-id");
             deleteBookModal.style.display = 'block';
-
-            confirmDeleteButton.onclick = function() {
-                fetch('/removeBook', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        id: bookId
-                    })
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.status === "success") {
-                            modalMessageBox.innerHTML = `<p style="color: green">${data.message}</p>`;
-                            confirmDeleteButton.disabled = true; // Wyłączenie przycisku potwierdzenia
-                            cancelDeleteButton.disabled = true; // Wyłączenie przycisku anulowania
-
-                            // Dodajmy nasłuchiwanie na kliknięcie poza obszarem modalu tylko po sukcesie operacji
-                            deleteBookModal.addEventListener("click", function(event) {
-                                if (event.target === deleteBookModal) {
-                                    closeDeleteModal();
-                                    refreshAfterDelay(1000); // odświeżenie strony po 1 sekundzie od zamknięcia modalu
-                                }
-                            });
-
-                            refreshAfterDelay(3000); // domyślne odświeżenie strony po 3 sekundach
-                        } else {
-                            modalMessageBox.innerHTML = `<p style="color: red">${data.message}</p>`;
-                        }
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            };
         });
+    });
+
+    confirmDeleteButton.addEventListener("click", function() {
+        fetch('/removeBook', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: bookId
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === "success") {
+                    modalMessageBox.innerHTML = `<p style="color: green">${data.message}</p>`;
+                    confirmDeleteButton.disabled = true;
+                    cancelDeleteButton.disabled = true;
+
+                    // Usuń cały element book-entry
+                    const bookEntry = document.querySelector(`.delete-btn[data-id="${bookId}"]`).closest('.book-entry');
+                    if (bookEntry) {
+                        bookEntry.remove();
+                    }
+
+                    deleteBookModal.addEventListener("click", function(event) {
+                        if (event.target === deleteBookModal) {
+                            closeDeleteModal();
+                        }
+                    });
+                } else {
+                    modalMessageBox.innerHTML = `<p style="color: red">${data.message}</p>`;
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                modalMessageBox.innerHTML = `<p style="color: red">Wystąpił błąd. Spróbuj ponownie później.</p>`;
+            });
     });
 
     deleteBookModal.addEventListener("click", function(event) {
--- public/js/remove-user.js
+++ public/js/remove-user.js
@@ -5,9 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const cancelDeleteButton = document.getElementById('cancelDeleteUser');
     const modalMessageBox = deleteUserModal.querySelector('.modal-messageBox');
 
+    let userId;
+
     function closeDeleteModal() {
         deleteUserModal.style.display = 'none';
         modalMessageBox.innerHTML = ''; // Wyczyszczenie komunikatu
+        confirmDeleteButton.disabled = false;
+        cancelDeleteButton.disabled = false;
     }
 
     // Obsługa przycisku zamykania modala
@@ -20,51 +24,50 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function refreshAfterDelay(delay) {
-        setTimeout(() => location.reload(), delay);
-    }
-
     // Dodawanie obsługi dla każdego przycisku "Usuń" dla użytkownika
     document.querySelectorAll('button[data-action="delete"]').forEach(function(button) {
         button.addEventListener("click", function() {
-            const userId = this.getAttribute("data-id");
+            userId = this.getAttribute("data-id");
             deleteUserModal.style.display = 'block';
-
-            confirmDeleteButton.onclick = function() {
-                fetch('/removeUser', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        id: userId
-                    })
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.status === "success") {
-                            modalMessageBox.innerHTML = `<p style="color: green">${data.message}</p>`;
-                            confirmDeleteButton.disabled = true; // Wyłączenie przycisku potwierdzenia
-                            cancelDeleteButton.disabled = true; // Wyłączenie przycisku anulowania
-
-                            // Dodajmy nasłuchiwanie na kliknięcie poza obszarem modalu tylko po sukcesie operacji
-                            deleteUserModal.addEventListener("click", function(event) {
-                                if (event.target === deleteUserModal) {
-                                    closeDeleteModal();
-                                    refreshAfterDelay(1000); // odświeżenie strony po 1 sekundzie od zamknięcia modalu
-                                }
-                            });
-
-                            refreshAfterDelay(3000); // domyślne odświeżenie strony po 3 sekundach
-                        } else {
-                            modalMessageBox.innerHTML = `<p style="color: red">${data.message}</p>`;
-                        }
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            };
         });
+    });
+
+    confirmDeleteButton.addEventListener("click", function() {
+        fetch('/removeUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: userId
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === "success") {
+                    modalMessageBox.innerHTML = `<p style="color: green">${data.message}</p>`;
+                    confirmDeleteButton.disabled = true; // Wyłączenie przycisku potwierdzenia
+                    cancelDeleteButton.disabled = true; // Wyłączenie przycisku anulowania
+
+                    // Usunięcie wiersza z tabeli
+                    const userRow = document.querySelector(`button[data-id="${userId}"]`).closest('tr');
+                    if (userRow) {
+                        userRow.remove();
+                    }
+
+                    deleteUserModal.addEventListener("click", function(event) {
+                        if (event.target === deleteUserModal) {
+                            closeDeleteModal();
+                        }
+                    });
+                } else {
+                    modalMessageBox.innerHTML = `<p style="color: red">${data.message}</p>`;
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                modalMessageBox.innerHTML = `<p style="color: red">Wystąpił błąd. Spróbuj ponownie później.</p>`;
+            });
     });
 
     deleteUserModal.addEventListener("click", function(event) {
